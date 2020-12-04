2b88f19f5023f7bb2c2bb939aeb5221a0a540846
Pablo Miro <paramiru@gmail.com>
2020-12-04 15:46:36 +0000

    Move code to other classes

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -1,31 +1,13 @@
 package uk.ac.ed.inf.aqmaps;
 
-
-import java.time.LocalDate;
-
-import com.mapbox.geojson.Point;
-
 public class App {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		var server = new ServerConnection(args);
+		var noFlyZones = server.getNoFlyZones();
+		System.out.println(noFlyZones);
+		// TODO 
+		// var sensors = server.getSensorsToVisit();
 		
-		var day = Integer.parseInt(args[0]);
-		var month = Integer.parseInt(args[1]);
-		var year = Integer.parseInt(args[2]);
-		var date = LocalDate.of(year, month, day);
-		System.out.println(date);
-		System.out.println(date.getDayOfMonth());
-		
-		
-		System.out.println(date.getMonthValue());
-		System.out.println(date.getYear());
-		
-		var latitude = Double.parseDouble(args[3]);
-		var longitude = Double.parseDouble(args[4]);
-		var starting_point = Point.fromLngLat(longitude, latitude);
-		
-		var seed = args[5];
-		var port = args[6];
-		var uri = "http://localhost:" + port;
 	}
 }
