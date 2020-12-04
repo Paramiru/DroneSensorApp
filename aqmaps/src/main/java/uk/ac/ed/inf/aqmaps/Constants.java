0d08ba8b60015631c9042b9826e456d47ab66245
Pablo Miro <paramiru@gmail.com>
2020-12-04 15:46:51 +0000

    Update constants

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Constants.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Constants.java
@@ -4,12 +4,11 @@ import com.mapbox.geojson.Point;
 
 public class Constants {
 
+	public static final String SERVER = "http://localhost:";
 	public static final Point TOP_LEFT = Point.fromLngLat(-3.192473, 55.946233);
 	public static final Point TOP_RIGHT = Point.fromLngLat(-3.184319, 55.946233);
 	public static final Point BOTTOM_LEFT = Point.fromLngLat(-3.192473, 55.942617);
 	public static final Point BOTTOM_RIGHT = Point.fromLngLat(-3.184319, 55.942617);
-	public static final int NO_FLY_ZONES = 0;
-	
 
 
 }
