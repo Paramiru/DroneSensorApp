9070977c435f4b3315fdb73a6edf6464fc8c30ed
Pablo Miro <paramiru@gmail.com>
2020-12-04 12:34:46 +0000

    Add parsing of CL arguments

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -1,8 +1,31 @@
 package uk.ac.ed.inf.aqmaps;
 
+
+import java.time.LocalDate;
+
+import com.mapbox.geojson.Point;
+
 public class App {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		var day = Integer.parseInt(args[0]);
+		var month = Integer.parseInt(args[1]);
+		var year = Integer.parseInt(args[2]);
+		var date = LocalDate.of(year, month, day);
+		System.out.println(date);
+		System.out.println(date.getDayOfMonth());
+		
+		
+		System.out.println(date.getMonthValue());
+		System.out.println(date.getYear());
+		
+		var latitude = Double.parseDouble(args[3]);
+		var longitude = Double.parseDouble(args[4]);
+		var starting_point = Point.fromLngLat(longitude, latitude);
+		
+		var seed = args[5];
+		var port = args[6];
+		var uri = "http://localhost:" + port;
 	}
 }
