27f0604585369300ef114a962738821ead0669d2
Pablo Miro <paramiru@gmail.com>
2020-12-04 12:35:27 +0000

    Points to represent confinement area

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Constants.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Constants.java
@@ -1,12 +1,15 @@
 package uk.ac.ed.inf.aqmaps;
 
+import com.mapbox.geojson.Point;
+
 public class Constants {
 
-//	public static final int TOP_LEFT;
-//	public static final int TOP_RIGHT;
-//	public static final int BOTTOM_LEFT;
-//	public static final int BOTTOM_RIGHT;
-//	public static final int NO_FLY_ZONES;
+	public static final Point TOP_LEFT = Point.fromLngLat(-3.192473, 55.946233);
+	public static final Point TOP_RIGHT = Point.fromLngLat(-3.184319, 55.946233);
+	public static final Point BOTTOM_LEFT = Point.fromLngLat(-3.192473, 55.942617);
+	public static final Point BOTTOM_RIGHT = Point.fromLngLat(-3.184319, 55.942617);
+	public static final int NO_FLY_ZONES = 0;
+	
 
 
 }
