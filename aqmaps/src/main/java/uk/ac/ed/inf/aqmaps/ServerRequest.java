6dd5da50f7c0deaf53a12b85a3e5d6e6bc8797a7
Pablo Miro <paramiru@gmail.com>
2020-12-05 13:18:04 +0000

    Rename class

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/ServerRequest.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/ServerRequest.java
@@ -1,33 +1,59 @@
 package uk.ac.ed.inf.aqmaps;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.ConnectException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ServerConnection {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+
+public class ServerRequest {
 	
 	private static final HttpClient client = HttpClient.newHttpClient(); 
 	
-	public ServerConnection(String[] args) {
+	public ServerRequest(String[] args) {
 		IO.parseArguments(args);
 	}
 	
-	protected String getNoFlyZones() throws InterruptedException {
+	protected List<Feature> getNoFlyZones() throws InterruptedException {
 		var path = "/buildings/no-fly-zones.geojson";
-		return getRequest(path);
+		var source = getRequest(path);
+		var fc = FeatureCollection.fromJson(source);
+		var features = fc.features();
+		return features;
 	}
 	
-	protected String getSensorsToVisit() throws InterruptedException {
+	protected ArrayList<Sensor> getSensors() throws InterruptedException {
 		var path = "/maps/" + IO.date.getYear() + "/" + IO.date.getMonth() 
 			+ "/" + IO.date.getDay() + "/" + "air-quality-data.json";
-		return getRequest(path);
 		
+		var source = getRequest(path);
+		
+		Type listType = new TypeToken<ArrayList<Sensor>>() {}.getType();
+		ArrayList<Sensor> sensors = new Gson().fromJson(source, listType);
+		return sensors;
+	}
+	
+	protected static WordsDetails getWordsAddress(String words) throws InterruptedException {
+		var splittedWords = words.split("\\.");
+		var path = "/words/" + splittedWords[0] + "/" + splittedWords[1] 
+		+ "/" + splittedWords[2] + "/" + "details.json";
+	
+		var source = getRequest(path);
+		
+		var address = new Gson().fromJson(source, WordsDetails.class);
+		return address;
 	}
 
-	protected String getRequest(String path) throws InterruptedException {
+	protected static String getRequest(String path) throws InterruptedException {
 		var urlString = Constants.SERVER + IO.port + path;
 		var request = HttpRequest.newBuilder()
 				.uri(URI.create(urlString))
@@ -54,8 +80,8 @@ public class ServerConnection {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		return responseBody;
 	}
 	
+
 }
