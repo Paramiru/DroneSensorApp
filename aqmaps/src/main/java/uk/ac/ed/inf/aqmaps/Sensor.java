eaf08393f8ae7a29f7076a8f9c3367551ca3acae
Pablo Miro <paramiru@gmail.com>
2020-12-05 17:57:47 +0000

    Add new functionality to senso

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -2,35 +2,39 @@ package uk.ac.ed.inf.aqmaps;
 
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.Geometry;
+import com.mapbox.geojson.Point;
 
 public class Sensor {
 	// location --> What3Words encoding
-	private String location;
+	protected String location;
 	// 0.0 <= battery <= 100.0
 	private float battery;
 	// 0.0 <= reading <= 255.0
 	private String reading;
 	
-	protected Feature getSensorAsFeature() throws InterruptedException {
+	protected Point getPointFromSensor() throws InterruptedException {
 		var point = ServerRequest.getWordsAddress(location).getCoordsAsPoint();
+		return point;
+	}
+	
+	protected Feature getSensorAsFeature() throws InterruptedException {
+		var colour = Marker.get_colour(this.battery, Float.parseFloat(this.reading));
+		var symbol = Marker.getSymbol(this.battery, Float.parseFloat(this.reading));
+		var point = getPointFromSensor();
 		var feature = Feature.fromGeometry((Geometry) point);
 		feature.addStringProperty("marker-size", "medium");
 		feature.addStringProperty("location", this.location);
-		var colour = Marker.get_colour(this.battery, Float.parseFloat(this.reading));
 		feature.addStringProperty("rgb-string", colour);
 		feature.addStringProperty("marker-color", colour);
-		var symbol = Marker.getSymbol(this.battery, Float.parseFloat(this.reading));
 		feature.addStringProperty("marker-symbol", symbol);
 
 		return feature;
 	}
 	
-
-	
 	@Override
 	public String toString() {
-		return "Sensor at \t" + location + "\twith battery\t" 
-				+ battery + "\tand reading\t" + reading;
+		return "Sensor at \t" + location;
 	}
+
 	
 }
