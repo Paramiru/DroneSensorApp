1b6c577def7248e66acea6458f80adf2d24fb722
Pablo Miro <paramiru@gmail.com>
2020-12-05 13:19:56 +0000

    Deserialize sensor into class

---
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -0,0 +1,34 @@
+package uk.ac.ed.inf.aqmaps;
+
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Geometry;
+
+public class Sensor {
+	// location --> What3Words encoding
+	private String location;
+	// 0.0 <= battery <= 100.0
+	private float battery;
+	// 0.0 <= reading <= 255.0
+	private String reading;
+	
+	protected Feature getSensorAsFeature() throws InterruptedException {
+		var point = ServerRequest.getWordsAddress(location).getCoordsAsPoint();
+		var feature = Feature.fromGeometry((Geometry) point);
+		feature.addStringProperty("marker-size", "medium");
+		feature.addStringProperty("location", this.location);
+		feature.addStringProperty("rgb-string", Colour.get_colour(this.battery));
+//		TODO
+//		feature.addStringProperty("marker-symbol", value);
+		
+		return feature;
+	}
+	
+
+	
+	@Override
+	public String toString() {
+		return "Sensor at \t" + location + "\twith battery\t" 
+				+ battery + "\tand reading\t" + reading;
+	}
+	
+}
