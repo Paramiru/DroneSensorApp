5e8e6c60a72e75370c071a51d544e2ad7c264533
Pablo Miro <paramiru@gmail.com>
2020-12-05 13:36:54 +0000

    Change to try out Colour.java class

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -1,11 +1,23 @@
 package uk.ac.ed.inf.aqmaps;
 
+import java.util.ArrayList;
+
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+
 public class App {
 	
 	public static void main(String[] args) throws InterruptedException {
 		var server = new ServerRequest(args);
-		var noFlyZones = server.getNoFlyZones();
+//		var noFlyZones = server.getNoFlyZones();
 		var sensors = server.getSensors();
+		var features = new ArrayList<Feature>();
+		for (Sensor sensor : sensors) {
+			features.add(sensor.getSensorAsFeature());
+		}
+		var featureCollection = FeatureCollection.fromFeatures(features);
+		var jsonString = featureCollection.toJson();
+		System.out.println(jsonString);
 
 	}
 }
