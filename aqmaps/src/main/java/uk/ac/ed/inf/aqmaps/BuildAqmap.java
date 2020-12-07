07e23b16caa86738b97ca6fee69908ad2db6f10c
Pablo Miro <paramiru@gmail.com>
2020-12-07 18:43:47 +0000

    Finish javadoc in BuildAqmap.java

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/BuildAqmap.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/BuildAqmap.java
@@ -24,7 +24,14 @@ public class BuildAqmap {
 	private static Sensor nextSensorToVisit;
 	private static Location nextGoalLocationForTheDrone;
 	
-
+	/**
+	 * Sets the server up so that we retrieve the sensors,
+	 * the no fly zones and initialises the variables
+	 * we will use later.
+	 * 
+	 * @param args command line arguments
+	 * @throws InterruptedException
+	 */
 	protected static void setUpMap(String[] args) throws InterruptedException {
 //		System.out.println("Setting up server...");
 		server = new ServerRequest(args);
@@ -39,6 +46,42 @@ public class BuildAqmap {
 		nextGoalLocationForTheDrone = nextSensorToVisit.getLocationFromSensor();
 	}
 	
+	/**
+	 * Returns the most optimal move for the drone to
+	 * take so that it gets closer to the next sensor
+	 * (or starting position) taking into consideration
+	 * the constraints (no fly zones and confined area)
+	 * 
+	 * It also takes into account the previous moves so 
+	 * that the drone does not go back and forth to the
+	 * same place wasting its moves. However, if the 
+	 * only possiblity is going back, it will do so since
+	 * there are some special cases which have to be taken
+	 * into account (imagine going to a zone which has the
+	 * same place to enter and exit.
+	 * 
+	 * @return most optimal move
+	 */
+	protected static Move makeOptimalMove() {
+		var possibleMoves = BuildAqmapUtils.getPossibleMoves(currentLocation);
+		BuildAqmapUtils.filterPossibleMoves(currentLocation, possibleMoves);
+		
+		var newPossibleMoves = possibleMoves;
+		newPossibleMoves.removeIf(pair -> BuildAqmapUtils.isGoingToPreviousPosition(pair));
+		if (newPossibleMoves.size() > 0) { possibleMoves = newPossibleMoves; }
+		
+		var optimalMove = BuildAqmapUtils.getOptimalMove(currentLocation, nextGoalLocationForTheDrone, moveNumber, possibleMoves);
+		return optimalMove;
+	}
+	
+	/**
+	 * makes the drone go from one sensor to the next
+	 * as long as it has enough moves or it has not 
+	 * finished checking every sensor retrieved from
+	 * the WebServer.
+	 * 
+	 * @throws InterruptedException
+	 */
 	protected static void collectSensors() throws InterruptedException {
 //		System.out.println("Drone starting to move beep beep beep");
 		while (moveNumber <= Constants.MAX_MOVES && nextSensorToVisit != null) {
@@ -54,11 +97,20 @@ public class BuildAqmap {
 				nextSensorToVisit = sensorsToVisit.poll();
 				if (nextSensorToVisit != null ) { nextGoalLocationForTheDrone = nextSensorToVisit.getLocationFromSensor(); }
 			}
+			// Adds move to the list which we will print in the end
 			chosenMoves.add(optimalMove);
 			points.add(optimalMove.getEndLocation().getGeojsonPoint());
 			moveNumber++;
 		}
 	}
+	
+	/**
+	 * If the drone has not done the maximum number of moves it can do,
+	 * try to make it go back to the origin. We do so in a similar way
+	 * to how we go to the sensors, but this time we consider a different
+	 * length from the origin (Constants.MOVE_LENGTH instead of 
+	 * Constants.SENSOR_DISTANCE) to finish.
+	 */
 	protected static void goBackToStartingLocation() {
 		// Tell the drone it must focus on the starting Location
 		nextGoalLocationForTheDrone = IO.startingPoint;
@@ -76,6 +128,17 @@ public class BuildAqmap {
 			moveNumber++;
 		}
 	}
+	
+	/**
+	 * Returns the corresponding GeoJson map taking into account the points
+	 * through which the drone has gone through (so that we can render the
+	 * path as a LineString), the sensors it has visited and also the ones
+	 * it has not visited (which will be grey-coloured and without any
+	 * symbol.
+	 * 
+	 * @return json string of the corresponding map
+	 * @throws InterruptedException
+	 */
 	protected static String createGeojsonMap() throws InterruptedException {
 		var lineString = LineString.fromLngLats(points);
 		var features = Utils.createFeaturesList(visitedSensors, lineString);
@@ -91,11 +154,23 @@ public class BuildAqmap {
 		return jsonString;
 	}
 	
+	/**
+	 * Writes the two requested files in the coursework.
+	 * 
+	 * @param jsonString string representing the GeoJson map
+	 */
 	protected static void writeFiles(String jsonString) {
 		IO.writeReadingFile(jsonString);
 		IO.writeFlightpathFile(chosenMoves);
 	}
 	
+	/**
+	 * Method which completes the map collecting the sensors
+	 * and trying to go back to the origin using the methods
+	 * previously defined.
+	 * 
+	 * @throws InterruptedException
+	 */
 	protected static void buildMap() throws InterruptedException {
 		collectSensors();
 //		System.out.println("Finished checking Sensors");
@@ -106,78 +181,5 @@ public class BuildAqmap {
 		
 	}
 	
-	protected static Move makeOptimalMove() {
-		var possibleMoves = BuildAqmapUtils.getPossibleMoves(currentLocation);
-		BuildAqmapUtils.filterPossibleMoves(currentLocation, possibleMoves);
-		
-		var newPossibleMoves = possibleMoves;
-		newPossibleMoves.removeIf(pair -> BuildAqmapUtils.isGoingToPreviousPosition(pair));
-		if (newPossibleMoves.size() > 0) { possibleMoves = newPossibleMoves; }
-		
-		var optimalMove = BuildAqmapUtils.getOptimalMove(currentLocation, nextGoalLocationForTheDrone, moveNumber, possibleMoves);
-		return optimalMove;
-	}
-//	protected static Location getEndLocation(Location start, Integer angle) {
-//		// given start location and angle, calculate what the end location will be
-//		var angleInRadians = Math.toRadians(angle);
-//		var newLatitude = start.latitude() + Constants.MOVE_LENGTH * Math.sin(angleInRadians);
-//		var newLongitude = start.longitude() + Constants.MOVE_LENGTH * Math.cos(angleInRadians);
-//		var endLocation = new Location(newLatitude, newLongitude);
-//		return endLocation;
-//	}
-//	protected static boolean isInConfinedArea(Location location) {
-//		var point = location.getJtsPoint();
-//		return point.within(Constants.confinedArea);
-//	}
-//	protected static boolean doesIntersectWithNoFlyZones(Location start, Location end) {
-//		var coordinates = new Coordinate[] {start.getJtsCoordinate(), end.getJtsCoordinate()};
-//		var line = Utils.geometryFactory.createLineString(coordinates);
-//		for (NoFlyZone zone : noFlyZones) {
-//			if (line.intersects(zone.getJtsPolygon())) return true;
-//		}
-//		return false;
-//	}
-//	protected static boolean isGoingToPreviousPosition(Pair<Integer, Location> possibleMove) {
-//		
-//		for (Move previousMove : chosenMoves) {
-//			var possibleEndLocation = possibleMove.getValue1();
-//			var distanceToPossibleEndLocation = Utils.getDistance(previousMove.getEndLocation(), possibleEndLocation);
-//			// if end location is too close to a previous move's end 
-//			// location then do not consider that possible move
-//			if (distanceToPossibleEndLocation < Constants.EPSILON) return true;
-//		}
-//		return false;
-//	}
-//	protected static void filterPossibleMoves(Location start, List<Pair<Integer, Location>> possibleMoves) {
-//		possibleMoves.removeIf(
-//				pair -> !isInConfinedArea(pair.getValue1())  
-//				|| doesIntersectWithNoFlyZones(start, pair.getValue1()) 
-//				);
-//	}
-//	protected static List<Pair<Integer, Location>> getPossibleMoves(Location start) {
-//		var possibleMoves = new ArrayList<Pair<Integer, Location>>();
-//		for (int angle = 0; angle <= 350; angle += 10) {
-//			var endLocation = getEndLocation(start, angle); 
-//			var move = new Pair<Integer, Location>(angle, endLocation);
-//			possibleMoves.add(move);
-//		}
-//		return possibleMoves;
-//	}
-//	protected static Move getOptimalMove(Location moveStartLocation, Location nextSensorLocation, Integer moveNumber, List<Pair<Integer, Location>> filteredPossibleMoves) {
-//		
-//		Move optimalMove = null;
-//		var minDistance = Double.MAX_VALUE;
-//		
-//		for (Pair<Integer, Location> move : filteredPossibleMoves) {
-//			var moveEndLocation = move.getValue1();
-//			var distanceFromMoveToSensor = Utils.getDistance(moveEndLocation, nextSensorLocation);
-//			
-//			if (distanceFromMoveToSensor < minDistance) {
-//				minDistance = distanceFromMoveToSensor;
-//				optimalMove = new Move(moveStartLocation, moveEndLocation, moveNumber, move.getValue0());
-//			}
-//			
-//		}
-//		return optimalMove;
-//	}
+	
 }
