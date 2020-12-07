a07be36fb4dd93bb40eb151cdc49047d8b71cd9f
Pablo Miro <paramiru@gmail.com>
2020-12-07 20:55:07 +0000

    Remove some unnecessary blank spaces

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -3,7 +3,6 @@ package uk.ac.ed.inf.aqmaps;
 public class App {
 	
 	public static void main(String[] args) throws InterruptedException {
-		
 		BuildAqmap.setUpMap(args);
 		BuildAqmap.buildMap();
 	}
